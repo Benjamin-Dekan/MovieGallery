f69f1c849bfd6968461760fd9de5227a3f70cf0b
Benjamin-Dekan <146146637+Benjamin-Dekan@users.noreply.github.com>
2026-09-06 19:26:53 -0400

    feat: add authentication check to profile page

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,7 +1,15 @@
-export default function ProfilePage() {
+import SignIn from "@/app/ui/signin-button";
+import SignOut from "@/app/ui/signout-button";
+import { auth } from "@/auth";
+
+export default async function ProfilePage() {
+  const session = await auth();
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Profile</h1>
-    </div>
+    <main>
+      <h1 className="text-3xl font-bold mb-3">Profile</h1>
+
+      <div>{session ? <SignOut /> : <SignIn />}</div>
+    </main>
   );
 }
